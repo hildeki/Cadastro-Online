05ee83504216af0ec14d20da9ed8a77e9d055b68
Hideki <lucashidekitogo0227@gmail.com>
2026-05-31 16:07:00 -0300

    Entrega do formulario

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ const clientForm = document.getElementById("clientForm");
 const nameInput = document.getElementById("nameInput");
 const emailInput = document.getElementById("emailInput");
 const dataList = document.getElementById("dataList");
+
+// Entrega do formulario
+clientForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const name = nameInput.value;
+    const email = emailInput.value;
+
+    console.log(name);
+    console.log(email);
+});
