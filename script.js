b589a2571445eaa8dcd25bc54b944eee67d77497
Hideki <lucashidekitogo0227@gmail.com>
2026-05-31 15:59:43 -0300

    Captura de elementos no JS

---
+++ script.js
@@ -0,0 +1,5 @@
+// Captura de elementos
+const clientForm = document.getElementById("clientForm");
+const nameInput = document.getElementById("nameInput");
+const emailInput = document.getElementById("emailInput");
+const dataList = document.getElementById("dataList");
