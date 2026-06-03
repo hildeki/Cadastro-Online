2853734fead205aa8174ef34f6b0510c708e4754
Hideki <lucashidekitogo0227@gmail.com>
2026-06-03 18:38:54 -0300

    Config de API

--- script.js
+++ script.js
@@ -4,8 +4,20 @@ const nameInput = document.getElementById("nameInput");
 const emailInput = document.getElementById("emailInput");
 const dataList = document.getElementById("dataList");
 
+// URL API
+const API_URL = "https://crudcrud.com/api/0f2c5f73f7874fd5812dcdd4b0061bc1/clients";
+
+// API dados sync
+async function createClient(name, email) {
+
+    // Requisicao de API
+     await fetch(API_URL, {
+        method: "POST"
+});
+}
+
 // Quando o formulario for enviado
-clientForm.addEventListener("submit", (event) => {
+clientForm.addEventListener("submit", async (event) => {
     
     // Impedir o comportamento padrão
     event.preventDefault();
@@ -15,6 +27,9 @@ clientForm.addEventListener("submit", (event) => {
     
     // Criar uma variavel para email
     const email = emailInput.value;
+
+    // Execucao do API
+    await createClient(name, email);
 
     // Exibir os dois no console
     console.log(name);
